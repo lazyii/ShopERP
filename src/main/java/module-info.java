d3d83c22fd0acc58066e690e2be2931952441156
wyd <wyudi@qq.com>
2021-06-02 13:53:55 +0800

    coroutine sample

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+/**
+ * @author admin
+ * @date 2021-06-02 13:30:14
+ * @version 1.0 edit by admin at 2021-06-02 13:30:14
+ */
+module kfoenix.main {
+
+    requires kotlin.stdlib;
+
+    requires javafx.graphics;
+
+}
--- src/main/kotlin/org/rainday/kfoenix/Launcher.kt
+++ src/main/kotlin/org/rainday/kfoenix/Launcher.kt
@@ -1,16 +1,21 @@
 package org.rainday.kfoenix
 
 import javafx.application.Application
-import javafx.event.ActionEvent
 import javafx.event.EventHandler
+import javafx.geometry.Insets
+import javafx.geometry.Pos
+import javafx.scene.Node
 import javafx.scene.Scene
-import javafx.scene.control.Button
-import javafx.scene.layout.BorderPane
+import javafx.scene.input.MouseEvent
 import javafx.scene.layout.StackPane
+import javafx.scene.paint.Color
+import javafx.scene.shape.Circle
+import javafx.scene.text.Text
 import javafx.stage.Stage
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.actor
 import kotlinx.coroutines.javafx.JavaFx
-import kotlinx.coroutines.runBlocking
 
 
 /**
@@ -20,29 +25,62 @@ import kotlinx.coroutines.runBlocking
  * @description
  */
 class Launcher : Application() {
-    override fun start(primaryStage: Stage) {
-        println("start")
-        // TODO("Not yet implemented")
-        // Create a scene and place a button in the scene
-        primaryStage.title = "Hello World!"
-        val btn = Button()
-        btn.text = "Say 'Hello World'"
-        /*btn.setOnAction(object : EventHandler<ActionEvent?>() {
-            override fun handle(event: ActionEvent?) {
-                println("Hello World!")
-            }
-        })*/
+    override fun start(stage: Stage) {
+        val hello = Text("Hello World!").apply {
+            fill = Color.valueOf("#C0C0C0")
+        }
 
-        val root = BorderPane()
-        root.children.add(btn)
-        primaryStage.scene = Scene(root, 300.0, 250.0)
-        primaryStage.show()
-    }
+        val fab = Circle(20.0, Color.valueOf("#FF4081"))
 
-    override fun init() {
-        super.init()
-        println("init ")
+        val root = StackPane().apply {
+            children += hello
+            children += fab
+            StackPane.setAlignment(hello, Pos.CENTER)
+            StackPane.setAlignment(fab, Pos.BOTTOM_RIGHT)
+            StackPane.setMargin(fab, Insets(15.0))
+        }
+
+        val scene = Scene(root, 240.0, 380.0).apply {
+            fill = Color.valueOf("#303030")
+        }
+
+        stage.title = "Example"
+        stage.scene = scene
+        stage.show()
+        setup(hello, fab)
     }
+}
+
+fun Node.onClick(action: suspend (MouseEvent) -> Unit) {
+    val eventActor = GlobalScope.actor<MouseEvent>(Dispatchers.Main, capacity = Channel.CONFLATED) {
+        for (event in channel) action(event) // pass event to action
+    }
+    onMouseClicked = EventHandler { event ->
+        eventActor.trySend(event)
+    }
+}
+
+fun setup(hello: Text, fab: Circle) {
+    var result = "none" // the last result
+    // counting animation
+    GlobalScope.launch(Dispatchers.Main) {
+        var counter = 0
+        while (true) {
+            hello.text = "${++counter}: $result"
+            // update the text every 100ms
+            delay(100)
+        }
+    }
+    // compute next fibonacci number of each click
+    var x = 1
+    fab.onClick {
+        result = "fib($x) = ${fib(x)}"
+        x++
+    }
+}
+
+suspend fun fib(x: Int): Int = withContext(Dispatchers.Main) {
+    if (x <= 1) x else fib(x - 1) + fib(x - 2)
 }
 
 fun main(args: Array<String>) {
